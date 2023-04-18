6da40159c25b7e0e72c93fa157b6ee1c4dc3c590
Sean <56064050+seangoats@users.noreply.github.com>
2023-04-18 22:12:55 +0800

    tests: add test for invalid amount

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -24,3 +24,29 @@ def test_withdraw_after_deposit():
     account.deposit(10)
     account.withdraw(5)
     assert account.balance == 5
+
+
+def test_withdraw_zero():
+    account = BankAccount()
+    account.deposit(10)
+    with pytest.raises(InvalidAmountException):
+        account.withdraw(0)
+
+
+def test_withdraw_negative():
+    account = BankAccount()
+    account.deposit(10)
+    with pytest.raises(InvalidAmountException):
+        account.withdraw(-10)
+
+
+def test_deposit_zero():
+    account = BankAccount()
+    with pytest.raises(InvalidAmountException):
+        account.withdraw(0)
+
+
+def test_deposit_negative():
+    account = BankAccount()
+    with pytest.raises(InvalidAmountException):
+        account.withdraw(-10)
